75a25c1d69ba748da73e65f88c1d24e5a73ea7a4
Robin Chang <sumdumghai@gmail.com>
2021-05-03 10:32:02 +0800

    Set custom relations between NPC mousekin factions

--- Source/FactionHelperExtension.cs
+++ Source/FactionHelperExtension.cs
@@ -1,9 +1,15 @@
-﻿using Verse;
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
 
 namespace MousekinRace
 {
     public class FactionHelperExtension : DefModExtension
     {
         public bool canSpawnSettlements = true;
+
+        public List<FactionDefStartingGoodwill> startingGoodwillByFactionDefs = new List<FactionDefStartingGoodwill>();
+
+        public List<FactionDef> rivalFactionTypes = new List<FactionDef>();
     }
 }
--- Source/MousekinRaceMod.cs
+++ Source/MousekinRaceMod.cs
@@ -3,6 +3,8 @@ using HarmonyLib;
 using RimWorld;
 using RimWorld.Planet;
 using System;
+using System.Collections.Generic;
+using UnityEngine;
 using Verse;
 
 namespace MousekinRace
@@ -25,6 +27,102 @@ namespace MousekinRace
             static bool Prefix(WorldObject o)
             {
                 return (o?.Faction?.def.GetModExtension<FactionHelperExtension>()?.canSpawnSettlements == false) ? false : true;
+            }
+        }
+
+        // Set up custom initial relations between NPC factions,
+        // by using recommended values set in the FactionHelperExtension mod extension
+        [HarmonyPatch(typeof(Faction), nameof(Faction.TryMakeInitialRelationsWith))]
+        static class Faction_TryMakeInitialRelationsWith_SetCustomRelationsBetweenNpcFactions
+        {
+            static FactionHelperExtension GetExtensionFor(Faction f)
+            {
+                return f.def.GetModExtension<FactionHelperExtension>();
+            }
+
+            static int MinOfNullableInts(int? num1, int? num2)
+            {
+                if (num1.HasValue && num2.HasValue)
+                {
+                    return (num1 < num2) ? (int)num1 : (int)num2;
+                }
+                if (num1.HasValue && !num2.HasValue)
+                {
+                    return (int)num1; 
+                }
+                if (!num1.HasValue && num2.HasValue)
+                {
+                    return (int)num2;
+                }
+                return 0;
+            }
+
+            static bool Prefix(Faction __instance, Faction other)
+            {
+                var currentFactionExt = GetExtensionFor(__instance);
+                var otherFactionExt = GetExtensionFor(other);
+
+                if ((__instance.RelationWith(other, allowNull: true) == null) && ((currentFactionExt?.startingGoodwillByFactionDefs.Exists(x => x.factionDef == other.def) ?? false) || (otherFactionExt?.startingGoodwillByFactionDefs.Exists(x => x.factionDef == __instance.def) ?? false)))
+                {
+                    int? currentToOtherFactionGoodwillMin = currentFactionExt.startingGoodwillByFactionDefs.Find(x => x.factionDef == other.def)?.Min ?? null;
+                    int? currentToOtherFactionGoodwillMax = currentFactionExt.startingGoodwillByFactionDefs.Find(x => x.factionDef == other.def)?.Max ?? null;
+                    int? otherToCurrentFactionGoodwillMin = otherFactionExt.startingGoodwillByFactionDefs.Find(x => x.factionDef == __instance.def)?.Min ?? null;
+                    int? otherToCurrentFactionGoodwillMax = otherFactionExt.startingGoodwillByFactionDefs.Find(x => x.factionDef == __instance.def)?.Max ?? null;
+                    Log.Warning(currentToOtherFactionGoodwillMin + ", "
+                            + currentToOtherFactionGoodwillMax + ", "
+                            + otherToCurrentFactionGoodwillMin + ", "
+                            + otherToCurrentFactionGoodwillMax
+                        );
+
+                    int mutualGoodwillMin = MinOfNullableInts(currentToOtherFactionGoodwillMin, otherToCurrentFactionGoodwillMin);
+
+                    int mutualGoodwillMax = MinOfNullableInts(currentToOtherFactionGoodwillMax, otherToCurrentFactionGoodwillMax);
+
+                    Log.Warning(__instance + "(" + __instance.def + ")" + " and " + other + "(" + other.def + ") have custom starting goodwill ranges! (" + mutualGoodwillMin + "~" + mutualGoodwillMax + ")");
+
+                    int finalMutualGoodWill = Rand.RangeInclusive(mutualGoodwillMin, mutualGoodwillMax);
+
+                    FactionRelationKind kind = (finalMutualGoodWill > -10) ? ((finalMutualGoodWill < 75) ? FactionRelationKind.Neutral : FactionRelationKind.Ally) : FactionRelationKind.Hostile;
+                    FactionRelation factionRelation = new FactionRelation
+                    {
+                        other = other,
+                        goodwill = finalMutualGoodWill,
+                        kind = kind
+                    };
+                    __instance.relations.Add(factionRelation);
+                    FactionRelation factionRelation2 = new FactionRelation
+                    {
+                        other = __instance,
+                        goodwill = finalMutualGoodWill,
+                        kind = kind
+                    };
+                    other.relations.Add(factionRelation2);
+
+                    return false;
+                }
+
+                /* TODO - Move the following commented out snippet to new faction rivalry system
+                 * 
+                 * if ((currentFactionExt?.rivalFactionTypes.Contains(other.def) ?? false) || (otherFactionExt?.rivalFactionTypes.Contains(__instance.def) ?? false))
+                {
+                    Log.Warning(__instance + " (" + __instance.def.label + ") and " + other + " (" + other.def.label + ") are rivals!");
+
+                    FactionRelationKind kind = FactionRelationKind.Hostile;
+                    FactionRelation factionRelation = new FactionRelation();
+                    factionRelation.other = other;
+                    factionRelation.goodwill = -80;
+                    factionRelation.kind = kind;
+                    __instance.relations.Add(factionRelation);
+                    FactionRelation factionRelation2 = new FactionRelation();
+                    factionRelation2.other = __instance;
+                    factionRelation2.goodwill = -80;
+                    factionRelation2.kind = kind;
+                    other.relations.Add(factionRelation2);
+
+                    return false;
+                }*/
+
+                return true;
             }
         }
     }
