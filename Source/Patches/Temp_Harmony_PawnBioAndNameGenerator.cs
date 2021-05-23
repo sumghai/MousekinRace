ce8102a0baea214ef4fa57e1f9a25173bfffe2d3
Robin Chang <sumdumghai@gmail.com>
2021-05-23 21:02:35 +0800

    (Debug) Log pawn gender during pawn generation

---
+++ Source/Patches/Temp_Harmony_PawnBioAndNameGenerator.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(PawnBioAndNameGenerator), nameof(PawnBioAndNameGenerator.FillBackstorySlotShuffled))]
+    public static class Temp_Harmony_PawnBioAndNameGenerator
+    {
+        static void Prefix(Pawn pawn, BackstorySlot slot)
+        {
+            Log.Warning("Generating " + slot + " bio for " + pawn + " (" + pawn.ageTracker.AgeBiologicalYears + ", " + pawn.gender + ", " + pawn.kindDef + ") of " + pawn.Faction + " (" + pawn.Faction.def + ")");
+            Log.Warning("---------");
+        }
+    }
+}
