a4f3a49ba7c010fb0d276f4ca4c8e24e03c1a353
Robin Chang <sumdumghai@gmail.com>
2021-05-28 21:39:05 +0800

    Add mood debuff for wearing Mousekin fur apparel

---
+++ Source/ThoughtWorker_WearingMousekinFurApparel.cs
@@ -0,0 +1,30 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MousekinRace
+{
+    public class ThoughtWorker_WearingMousekinFurApparel : ThoughtWorker
+    {
+        public override ThoughtState CurrentStateInternal(Pawn pawn)
+        {
+            // Mousekins are always Humanlike, so we can skip to directly checking if both pawns are Mousekins
+            if (!Utils.IsMousekin(pawn))
+            {
+                return ThoughtState.Inactive;
+            }
+
+            int num = 0;
+            List<Apparel> wornApparel = pawn.apparel.WornApparel;
+            for (int i = 0; i < wornApparel.Count; i++)
+            {
+                if (wornApparel[i].Stuff == MousekinDefOf.Mousekin.race.leatherDef)
+                {
+                    num++;
+                }
+            }
+
+            return (num > 0) ? ThoughtState.ActiveAtStage(0) : ThoughtState.Inactive;
+        }
+    }
+}
