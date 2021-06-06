8d27dd492997d3374e4ff017a931c20811c37f7f
Robin Chang <sumdumghai@gmail.com>
2021-06-06 14:07:59 +0800

    Force apparel made from cloth to use color generators

---
+++ Source/Patches/Harmony_GenRecipe.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(GenRecipe), nameof(GenRecipe.PostProcessProduct))]
+    public static class Harmony_GenRecipe_PostProcessProduct_SetCraftedApparelColor
+    {
+        static void Prefix(ref Thing product)
+        {
+            CompColorable compColorable = product.TryGetComp<CompColorable>();
+
+            if (compColorable != null && product.def.colorGenerator != null && (product.Stuff == null || product.Stuff.stuffProps.allowColorGenerators))
+            {
+                product.SetColor(product.def.colorGenerator.NewRandomizedColor());
+            }
+        }
+    }
+}
