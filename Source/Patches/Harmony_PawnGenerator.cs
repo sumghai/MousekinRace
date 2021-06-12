4d56572fc99238bb3548d68139a3fda73381c22b
Robin Chang <sumdumghai@gmail.com>
2021-06-12 19:52:04 +0800

    Block traits conflicting with each faith degree

---
+++ Source/Patches/Harmony_PawnGenerator.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(PawnGenerator), nameof(PawnGenerator.GeneratePawn), new Type[] { typeof(PawnGenerationRequest) })]
+    public static class Harmony_PawnGenerator_RemoveTraitsConflictingWithFaithSpectrum
+    {
+        static void Postfix(ref Pawn __result)
+        {
+            if (Utils.IsMousekin(__result))
+            {
+                int pawnFaithTraitDegree = __result.story.traits.DegreeOfTrait(MousekinDefOf.Mousekin_TraitSpectrum_Faith);
+
+                if (pawnFaithTraitDegree > 0)
+                {
+                    var traitConflictResolverExt = MousekinDefOf.Mousekin_TraitSpectrum_Faith.GetModExtension<TraitConflictResolverExtension>();
+
+                    traitConflictResolverExt.conflictingTraitsByFaithDegree.TryGetValue(pawnFaithTraitDegree, out List<TraitDef> conflictingTraits);
+
+                    if (conflictingTraits != null)
+                    {
+                        __result.story.traits.allTraits.RemoveAll(x => conflictingTraits.Contains(x.def));
+                    }
+                }
+            }           
+        }
+    }
+}
---
+++ Source/TraitConflictResolverExtension.cs
@@ -0,0 +1,11 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace MousekinRace
+{
+    public class TraitConflictResolverExtension : DefModExtension
+    {
+        public Dictionary<int, List<TraitDef>> conflictingTraitsByFaithDegree = new Dictionary<int, List<TraitDef>>();
+    }
+}
