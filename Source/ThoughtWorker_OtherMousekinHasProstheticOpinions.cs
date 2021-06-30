0ff75374d0ed3469f8b599fca2b4dd85e96fb081
Robin Chang <sumdumghai@gmail.com>
2021-06-30 21:21:59 +0800

    Add social opinions towards implants w.r.t. faith

---
+++ Source/ThoughtWorker_OtherMousekinHasProstheticOpinions.cs
@@ -0,0 +1,46 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MousekinRace
+{
+    public class ThoughtWorker_OtherMousekinHasProstheticOpinions : ThoughtWorker
+    {
+        public override ThoughtState CurrentSocialStateInternal(Pawn pawn, Pawn otherPawn)
+        {
+            // Mousekins are always Humanlike, so we can skip to directly checking if both pawns are Mousekins
+            if (!Utils.IsMousekin(pawn) || !Utils.IsMousekin(otherPawn))
+            {
+                return false;
+            }
+
+            if (!RelationsUtility.PawnsKnowEachOther(pawn, otherPawn))
+            {
+                return false;
+            }
+
+            // Degrees of Mousekin_TraitSpectrum_Faith:
+            //
+            // 0 = None / undefined
+            // 1 = Apostate
+            // 2 = Devotionist
+            // 3 = Pious
+            // 4 = Inquisitionist
+
+            int pawnFaithTraitDegree = pawn.story.traits.DegreeOfTrait(MousekinDefOf.Mousekin_TraitSpectrum_Faith);
+
+            if (pawnFaithTraitDegree < 2)
+            {
+                return false;
+            }
+
+            int numOfProstheticsOnOtherPawn = otherPawn.health.hediffSet.CountAddedAndImplantedParts();
+
+            if (numOfProstheticsOnOtherPawn < 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Source/Thought_OtherMousekinHasProstheticOpinions.cs
@@ -0,0 +1,43 @@
+﻿using RimWorld;
+
+namespace MousekinRace
+{
+    public class Thought_OtherMousekinHasProstheticOpinions : Thought_SituationalSocial
+    {
+		public override float OpinionOffset()
+		{
+			if (ThoughtUtility.ThoughtNullified(pawn, def))
+			{
+				return 0f;
+			}
+
+			// Degrees of Mousekin_TraitSpectrum_Faith:
+			//
+			// 0 = None / undefined
+			// 1 = Apostate
+			// 2 = Devotionist
+			// 3 = Pious
+			// 4 = Inquisitionist
+
+			int pawnFaithTraitDegree = pawn.story.traits.DegreeOfTrait(MousekinDefOf.Mousekin_TraitSpectrum_Faith);
+
+			// Nones and Apostates
+			if (pawnFaithTraitDegree <= 1)
+			{
+				return 0f;
+			}
+			// Devotionists
+			if (pawnFaithTraitDegree == 2)
+			{
+				return -5f;
+			}
+			// Pious
+			if (pawnFaithTraitDegree == 3)
+			{
+				return -15f;
+			}
+			// Inquisitionists
+			return -20f;
+		}
+	}
+}
