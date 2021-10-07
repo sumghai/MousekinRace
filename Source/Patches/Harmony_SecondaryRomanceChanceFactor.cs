84cc3fbeee75ee788668677275b87b03ba33598c
sumghai <sumdumghai@gmail.com>
2021-10-07 20:46:26 +0800

    Prevent new relationships for priests and nuns

---
+++ Source/Patches/Harmony_SecondaryRomanceChanceFactor.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(Pawn_RelationsTracker), nameof(Pawn_RelationsTracker.SecondaryRomanceChanceFactor))]
+    public static class Harmony_Pawn_RelationsTracker_SecondaryRomanceChanceFactor_PreventNewRomancesForRestrictedPawnKinds
+    {
+        public static void Postfix(Pawn otherPawn, Pawn ___pawn, ref float __result)
+        {
+            if ((___pawn.kindDef.GetModExtension<RomanceUtilsExtension>()?.preventNewRomances == true) || (otherPawn.kindDef.GetModExtension<RomanceUtilsExtension>()?.preventNewRomances == true))
+            {
+                __result = 0f;
+            }
+        }
+    }
+}
---
+++ Source/RomanceUtilsExtension.cs
@@ -0,0 +1,9 @@
+﻿using Verse;
+
+namespace MousekinRace
+{
+    public class RomanceUtilsExtension : DefModExtension
+    {
+        public bool preventNewRomances = false;
+    }
+}
