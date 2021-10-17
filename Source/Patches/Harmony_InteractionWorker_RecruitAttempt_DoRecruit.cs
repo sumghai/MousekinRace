e092714a86f45828f5822b1dc318d68c0736ba85
sumghai <sumdumghai@gmail.com>
2021-10-17 12:49:06 +0800

    Conditionally name giant cavies when tamed
    If a Mousekin is the one taming the giant cavy, the tamed animal should immediately use the custom namegiver to pick a name
    
    Co-Authored-By: Taranchuk <24846741+Taranchuk@users.noreply.github.com>

---
+++ Source/Patches/Harmony_InteractionWorker_RecruitAttempt_DoRecruit.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(InteractionWorker_RecruitAttempt), nameof(InteractionWorker_RecruitAttempt.DoRecruit), new Type[] { typeof(Pawn), typeof(Pawn), typeof(string), typeof(string), typeof(bool), typeof(bool) },
+        new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out, ArgumentType.Normal, ArgumentType.Normal })]
+    public static class Harmony_InteractionWorker_RecruitAttempt_DoRecruit_NameAnimalOnTame
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = instructions.ToList();
+            bool found = false;
+
+            for (var i = 0; i < codes.Count; i++)
+            {
+                // Find the method call where an unnamed wild animal is tamed, and add a conditional check (see below) 
+                if (!found && codes[i].opcode == OpCodes.Ldstr && codes[i].OperandIs("MessageTameAndNameSuccess"))
+                {
+                    found = true;
+                    yield return new CodeInstruction(OpCodes.Ldarg_0, null);
+                    yield return new CodeInstruction(OpCodes.Ldarg_1, null);
+                    yield return new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_InteractionWorker_RecruitAttempt_DoRecruit_NameAnimalOnTame), "ConditionallySetAnimalName", null, null));
+                }
+
+                yield return codes[i];
+            }
+        }
+
+        public static void ConditionallySetAnimalName(Pawn recruiter, Pawn recruitee)
+        {
+            // If the tamer is a Mousekin and the animal being tamed has a custom mod extension, immediately name the animal upon taming
+            if (Utils.IsMousekin(recruiter) && recruitee.kindDef.HasModExtension<NameAnimalOnTameExtension>())
+            {
+                recruitee.Name = PawnBioAndNameGenerator.GeneratePawnName(recruitee, NameStyle.Full, null);
+            }
+        }
+    }
+}
