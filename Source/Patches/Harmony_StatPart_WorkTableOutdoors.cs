0427168f1b41d7b1a5e642ca96e5cbceb74fecf1
sumghai <sumdumghai@gmail.com>
2022-02-21 20:45:02 +0800

    Remove outdoor and temperature work speed penalties for Smithy

---
+++ Source/Patches/Harmony_StatPart_WorkTableOutdoors.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    // Ignore outdoor work speed penalties for selected workbenches
+    [HarmonyPatch(typeof(StatPart_WorkTableOutdoors), "Applies", new Type[] { typeof(ThingDef), typeof(Map), typeof(IntVec3) })]
+    public class Harmony_StatPart_WorkTableOutdoors_IgnorePenalties
+    {
+        public static void Postfix(ref bool __result, ThingDef def)
+        {
+            __result = def.GetModExtension<BuildingIgnoreWorkSpeedPenaltiesExtension>() == null && __result;
+        }
+    }
+}
---
+++ Source/Patches/Harmony_StatPart_WorkTableTemperature.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    // Ignore outdoor work speed penalties for selected workbenches
+    [HarmonyPatch(typeof(StatPart_WorkTableTemperature), "Applies", new Type[] { typeof(ThingDef), typeof(Map), typeof(IntVec3) })]
+    public class Harmony_StatPart_WorkTableTemperature_IgnorePenalties
+    {
+        public static void Postfix(ref bool __result, ThingDef tDef)
+        {
+            __result = tDef.GetModExtension<BuildingIgnoreWorkSpeedPenaltiesExtension>() == null && __result;
+        }
+    }
+}
