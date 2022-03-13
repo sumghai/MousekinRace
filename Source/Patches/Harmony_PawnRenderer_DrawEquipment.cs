756ff8a960f8b3d805e45b844a90b577afa4c551
sumghai <sumdumghai@gmail.com>
2022-03-13 17:25:32 +0800

    Add custom position/rotation offsets for pikes when drafted

---
+++ Source/Patches/Harmony_PawnRenderer_DrawEquipment.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch(typeof(PawnRenderer), nameof(PawnRenderer.DrawEquipmentAiming))]
+    public static class Harmony_PawnRenderer_DrawEquipmentAiming
+    {
+		public static void Prefix(PawnRenderer __instance, Thing eq, ref Vector3 drawLoc, ref float aimAngle)
+		{
+			Pawn pawn = __instance.pawn;
+			WeaponRenderingExtension weaponRenderExt = eq.def.GetModExtension<WeaponRenderingExtension>();
+
+			if (__instance.CarryWeaponOpenly() && !pawn.stances.curStance.StanceBusy && weaponRenderExt != null)
+			{
+				if (pawn.Rotation == Rot4.South)
+				{
+					drawLoc -= new Vector3(0f, 0f, -0.22f) - weaponRenderExt.draftedDrawOffsets.south.posOffset;
+					aimAngle = weaponRenderExt.draftedDrawOffsets.south.angOffset;
+				}
+				else if (pawn.Rotation == Rot4.North)
+				{
+					drawLoc -= new Vector3(0f, 0f, -0.11f) - weaponRenderExt.draftedDrawOffsets.north.posOffset;
+					aimAngle = weaponRenderExt.draftedDrawOffsets.north.angOffset;
+				}
+				else if (pawn.Rotation == Rot4.East)
+				{
+					drawLoc -= new Vector3(0.2f, 0f, -0.22f) - weaponRenderExt.draftedDrawOffsets.east.posOffset;
+					aimAngle = weaponRenderExt.draftedDrawOffsets.east.angOffset;
+				}
+				else if (pawn.Rotation == Rot4.West)
+				{
+					drawLoc -= new Vector3(-0.2f, 0f, -0.22f) - weaponRenderExt.draftedDrawOffsets.west.posOffset;
+					aimAngle = weaponRenderExt.draftedDrawOffsets.west.angOffset;
+				}
+			}
+		}
+	}
+}
---
+++ Source/WeaponRenderingExtension.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace MousekinRace
+{
+    public class WeaponRenderingExtension : DefModExtension
+    {
+        public DraftedDrawOffsets draftedDrawOffsets = new();
+    }
+
+    public class DraftedDrawOffsets
+    {
+        public Offset north = new();
+        public Offset east = new();
+        public Offset south = new();
+        public Offset west = new();
+    }
+
+    public struct Offset
+    {
+        public Vector3 posOffset;
+        public float angOffset;
+    }
+}
