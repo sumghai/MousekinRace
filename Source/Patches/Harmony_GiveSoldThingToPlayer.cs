8ea4427ba2e35f53f86548d75a5f02cb9ffcf522
sumghai <sumdumghai@gmail.com>
2022-08-08 21:13:06 +1200

    Force cavies & pudgemice purchased from settlements to use namegens

--- Source/Patches/Harmony_GiveSoldThingToPlayer.cs
+++ Source/Patches/Harmony_GiveSoldThingToPlayer.cs
@@ -13,7 +13,8 @@ namespace MousekinRace.Patches
         static IEnumerable<MethodBase> TargetMethods()
         {
             yield return AccessTools.Method(typeof(Caravan_TraderTracker), nameof(Caravan_TraderTracker.GiveSoldThingToPlayer));
-            yield return AccessTools.Method(typeof(Pawn_TraderTracker), nameof(Pawn_TraderTracker.GiveSoldThingToPlayer));
+            yield return AccessTools.Method(typeof(Pawn_TraderTracker), nameof(Pawn_TraderTracker.GiveSoldThingToPlayer)); 
+            yield return AccessTools.Method(typeof(Settlement_TraderTracker), nameof(Settlement_TraderTracker.GiveSoldThingToPlayer));
             yield return AccessTools.Method(typeof(TradeShip), nameof(TradeShip.GiveSoldThingToPlayer));
         }
 
@@ -24,7 +25,7 @@ namespace MousekinRace.Patches
                 if (toGive is Pawn purchasedPawn && purchasedPawn.RaceProps.Animal)
                 {
                     if (purchasedPawn.Name == null && purchasedPawn.kindDef.HasModExtension<NameAnimalOnTameExtension>())
-                    { 
+                    {
                         purchasedPawn.Name = PawnBioAndNameGenerator.GeneratePawnName(purchasedPawn, NameStyle.Full, null);
                         Messages.Message("MousekinRace_MessagePurchasedAnimalNamed".Translate(purchasedPawn.def.label, purchasedPawn.Name.ToString()), purchasedPawn, MessageTypeDefOf.PositiveEvent, true);
                     }
