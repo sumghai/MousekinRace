6d331e2cae34f8ad21d70f62f6d7762d7bb5d3a8
sumghai <sumdumghai@gmail.com>
2022-08-08 20:44:11 +1200

    Force cavies & pudgemice purchased from caravan traders to use namegens

---
+++ Source/Patches/Harmony_GiveSoldThingToPlayer.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace MousekinRace.Patches
+{
+    [HarmonyPatch]
+    public class Harmony_GiveSoldThingToPlayer
+    {
+        static IEnumerable<MethodBase> TargetMethods()
+        {
+            yield return AccessTools.Method(typeof(Caravan_TraderTracker), nameof(Caravan_TraderTracker.GiveSoldThingToPlayer));
+            yield return AccessTools.Method(typeof(Pawn_TraderTracker), nameof(Pawn_TraderTracker.GiveSoldThingToPlayer));
+            yield return AccessTools.Method(typeof(TradeShip), nameof(TradeShip.GiveSoldThingToPlayer));
+        }
+
+        static void Prefix(Thing toGive, Pawn playerNegotiator)
+        {
+            if (Utils.GetRaceOfFaction(playerNegotiator.Faction.def) == MousekinDefOf.Mousekin)
+            {
+                if (toGive is Pawn purchasedPawn && purchasedPawn.RaceProps.Animal)
+                {
+                    if (purchasedPawn.Name == null && purchasedPawn.kindDef.HasModExtension<NameAnimalOnTameExtension>())
+                    { 
+                        purchasedPawn.Name = PawnBioAndNameGenerator.GeneratePawnName(purchasedPawn, NameStyle.Full, null);
+                        Messages.Message("MousekinRace_MessagePurchasedAnimalNamed".Translate(purchasedPawn.def.label, purchasedPawn.Name.ToString()), purchasedPawn, MessageTypeDefOf.PositiveEvent, true);
+                    }
+                }
+            }
+        }
+    }
+}
