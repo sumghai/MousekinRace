56c981de2a6e1fc22e4b719d820be56e6012dd26
sumghai <sumdumghai@gmail.com>
2022-10-04 21:35:34 +1300

    Add Mousekin candle sconce

---
+++ Source/Comps/CompProperties_FireOverlayDirectionalOffsets.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MousekinRace
+{
+    public class CompProperties_FireOverlayDirectionalOffsets : CompProperties_FireOverlay
+    {
+        public DirectionalOffsets directionalOffsets = null;
+
+        public CompProperties_FireOverlayDirectionalOffsets()
+        {
+            compClass = typeof(CompFireOverlayDirectionalOffsets);
+        }
+
+        public override void DrawGhost(IntVec3 center, Rot4 rot, ThingDef thingDef, Color ghostCol, AltitudeLayer drawAltitude, Thing thing = null)
+        {
+            GhostUtility.GhostGraphicFor(CompFireOverlay.FireGraphic, thingDef, ghostCol).DrawFromDef(center.ToVector3ShiftedWithAltitude(drawAltitude) + directionalOffsets.GetOffset(rot), rot, thingDef);
+        }
+    }
+
+    public class DirectionalOffsets
+    {
+        public Vector3 GetOffset(Rot4 rot) =>
+            rot == Rot4.South ? this.south :
+            rot == Rot4.North ? this.north :
+            rot == Rot4.East ? this.east : 
+            this.west != Vector3.zero ? this.west : new Vector3(-this.east.x, this.east.y, this.east.z);
+
+        public Vector3 north = Vector3.zero;
+        public Vector3 east = Vector3.zero;
+        public Vector3 south = Vector3.zero;
+        public Vector3 west = Vector3.zero;
+    }
+}
---
+++ Source/PlaceWorkers/PlaceWorker_WallMounted.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace MousekinRace
+{
+    public class PlaceWorker_WallMounted : PlaceWorker
+    {
+        public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
+        {
+            IntVec3 wallCell = center + rot.FacingCell;
+            GenDraw.DrawFieldEdges(new List<IntVec3>() { wallCell });
+        }
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            IntVec3 wallCell = loc + rot.FacingCell;
+            if (loc.Impassable(map) || !wallCell.InBounds(map) || !wallCell.Impassable(map))
+            {
+                return new AcceptanceReport("MousekinRace_PlaceWorker_MustBeWallMounted".Translate());
+            }
+            if (loc.GetDoor(map) != null)
+            {
+                return new AcceptanceReport("MousekinRace_PlaceWorker_CannotPlaceInDoorways".Translate());
+            }
+            return true;
+        }
+    }
+}
