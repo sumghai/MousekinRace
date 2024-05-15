a5833313b973e7c6505ae4bbb752c88257a2fb2e
sumghai <sumghai@users.noreply.github.com>
2024-05-15 19:14:23 +1200

    Allegiance System: Disable tribute collector if allegiance is set

---
+++ Source/Patches/Harmony_IncidentWorker_CaravanArrivalTributeCollector.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace MousekinRace
+{
+    // Disable Shattered Empire tribute collector if the player has pledged allegiance to a Mousekin faction
+    [HarmonyPatch(typeof(IncidentWorker_CaravanArrivalTributeCollector), nameof(IncidentWorker_CaravanArrivalTributeCollector.CanFireNowSub))]
+    public static class Harmony_IncidentWorker_CaravanArrivalTributeCollector_CanFireNowSub_DisableIfAllegianceSet
+    {
+        static void Postfix(ref bool __result)
+        {
+            if (GameComponent_Allegiance.Instance.alignedFaction != null) 
+            {
+                __result = false;
+            }
+        }
+    }
+}
