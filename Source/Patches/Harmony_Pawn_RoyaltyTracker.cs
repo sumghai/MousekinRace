9379a34d84b9eedf7d9c0703dd33eda953ad27a6
sumghai <sumghai@users.noreply.github.com>
2024-05-15 17:46:30 +1200

    Housekeeping: Rename Harmony patch to be more descriptive

--- Source/Patches/Harmony_Pawn_RoyaltyTracker.cs
+++ Source/Patches/Harmony_Pawn_RoyaltyTracker.cs
@@ -5,7 +5,7 @@ namespace MousekinRace
 {
     // Update a cached boolean in the Faction Allegiance system every time a pawn gains or loses a royal title with the Shattered Empire
     [HarmonyPatch(typeof(Pawn_RoyaltyTracker), nameof(Pawn_RoyaltyTracker.OnPostTitleChanged))]
-    public static class Harmony_Pawn_RoyaltyTracker
+    public static class Harmony_Pawn_RoyaltyTracker_OnPostTitleChanged_CheckForAnyPawnsWithEmpireTitle
     {
         static void Postfix()
         {
