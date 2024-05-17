8508c7efa8203ff709776f40281bac08fb07a547
sumghai <sumghai@users.noreply.github.com>
2024-05-18 08:16:04 +1200

    Refine hooded apparel logic for hiding Mousekin ears

--- Source/Patches/Harmony_AlienRace_ExtendedGraphics_ConditionApparel_Satisfied.cs
+++ Source/Patches/Harmony_AlienRace_ExtendedGraphics_ConditionApparel_Satisfied.cs
@@ -13,7 +13,7 @@ namespace MousekinRace
     {
         static void Postfix(ConditionApparel __instance, ref bool __result, ExtendedGraphicsPawnWrapper pawn, ref ResolveData data)
         {
-            if (pawn.WrappedPawn.apparel != null && ((!AlienRenderTreePatches.IsPortrait(pawn.WrappedPawn) && pawn.VisibleInBed()) || (AlienRenderTreePatches.IsPortrait(pawn.WrappedPawn) && !Prefs.HatsOnlyOnMap)))
+            if (__result && pawn.WrappedPawn.apparel != null && ((!AlienRenderTreePatches.IsPortrait(pawn.WrappedPawn) && pawn.VisibleInBed()) || (AlienRenderTreePatches.IsPortrait(pawn.WrappedPawn) && !Prefs.HatsOnlyOnMap)))
             {
                 if (pawn.GetWornApparel.FirstOrDefault(ap => ap.HasComp<CompApparelWithAttachedHeadgear>()) is Apparel hoodedApparel && hoodedApparel.GetComp<CompApparelWithAttachedHeadgear>() is CompApparelWithAttachedHeadgear comp && comp.Props.hideHarBodyAddonsWithTag.Any(s => __instance.hiddenUnderApparelTag.Contains(s)))
                 {
