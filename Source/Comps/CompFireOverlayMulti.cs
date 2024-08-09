be9c39eaf48adc7f97b0ead2a8bcef2275be5c4c
sumghai <sumghai@users.noreply.github.com>
2024-08-09 18:01:26 +1200

    Add Altar
    Not Ideology DLC compatible, just a basic implementation for the upcoming Church Services (name tbd) mechanic

---
+++ Source/Comps/CompFireOverlayMulti.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace MousekinRace
+{
+    [StaticConstructorOnStartup]
+    public class CompFireOverlayMulti : CompFireOverlay
+    {
+        public new CompProperties_FireOverlayMulti Props => (CompProperties_FireOverlayMulti)props;
+
+        public override void PostDraw()
+        {
+            if (refuelableComp == null || refuelableComp.HasFuel)
+            {
+                foreach (FireOffsetEntry fire in Props.fires) 
+                {
+                    Vector3 drawPos = parent.DrawPos + Props.DrawOffsetForRot(fire, parent.Rotation);
+                    drawPos.y += 1f / 26f;
+                    FireGraphic.Draw(drawPos, parent.Rotation, parent);
+                }
+            }
+        }
+    }
+}
---
+++ Source/Comps/CompProperties_FireOverlayMulti.cs
@@ -0,0 +1,48 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace MousekinRace
+{
+    public class CompProperties_FireOverlayMulti : CompProperties_FireOverlay
+    {
+        public List<FireOffsetEntry> fires;
+
+        public Vector3 DrawOffsetForRot(FireOffsetEntry fire, Rot4 rot)
+        {
+            return rot.AsInt switch
+            {
+                0 => fire.offsetNorth ?? fire.offset,
+                1 => fire.offsetEast ?? fire.offset,
+                2 => fire.offsetSouth ?? fire.offset,
+                3 => fire.offsetWest ?? fire.offset,
+                _ => fire.offset,
+            };
+        }
+
+        public override void DrawGhost(IntVec3 center, Rot4 rot, ThingDef thingDef, Color ghostCol, AltitudeLayer drawAltitude, Thing thing = null)
+        {
+            Graphic graphic = GhostUtility.GhostGraphicFor(CompFireOverlay.FireGraphic, thingDef, ghostCol);
+            foreach (FireOffsetEntry fire in fires)
+            {
+                Vector3 loc = center.ToVector3ShiftedWithAltitude(drawAltitude) + thingDef.graphicData.DrawOffsetForRot(rot) + DrawOffsetForRot(fire, rot);
+                graphic.DrawFromDef(loc, rot, thingDef);
+            }
+        }
+
+        public CompProperties_FireOverlayMulti()
+        {
+            compClass = typeof(CompFireOverlayMulti);
+        }
+    }
+
+    public class FireOffsetEntry 
+    {
+        public Vector3 offset;
+        public Vector3? offsetNorth;
+        public Vector3? offsetSouth;
+        public Vector3? offsetWest;
+        public Vector3? offsetEast;
+    }
+}
