35183fb6fe8496e932b9eb63bc6d6a20fc0c6154
sumghai <sumghai@users.noreply.github.com>
2024-09-24 20:43:19 +1200

    Ideology DLC: Disable role apparel requirements for Mousekin ideos

---
+++ Source/Patches/Harmony_Precept_Role.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+
+namespace MousekinRace
+{
+    // Disable role precept apparel requirements in Mousekin ideos
+    [HarmonyPatch(typeof(Precept_Role), nameof(Precept_Role.GenerateNewApparelRequirements))]
+    public static class Harmony_Precept_Role
+    {
+        static bool Prefix(ref List<PreceptApparelRequirement> __result, FactionDef generatingFor)
+        {
+            if (generatingFor.categoryTag != null && generatingFor.categoryTag.Contains("Mousekin")) 
+            {
+                __result = null;
+                return false;
+            }
+            return true;
+        }
+    }
+}
