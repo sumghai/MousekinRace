123cb3e24af8e884d1b7f5022a8eea112ed2c6f3
sumghai <sumghai@users.noreply.github.com>
2025-02-27 18:23:26 +1300

    Ideology DLC: Remove dup Town Square added by ideo precepts

---
+++ Source/Patches/Harmony_DesignationCategoryDef.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using Verse;
+
+namespace MousekinRace
+{
+    // Remove duplicate Town Square added by some Mousekin ideo building precepts from Architect Menu
+    [HarmonyPatch(typeof(DesignationCategoryDef), nameof(DesignationCategoryDef.AllIdeoDesignators), MethodType.Getter)]
+    public class Harmony_DesignationCategoryDef_AllIdeoDesignators_SkipDuplicateTownSquares
+    {
+        static IEnumerable<Designator> Postfix(IEnumerable<Designator> designators, DesignationCategoryDef __instance,
+            Dictionary<DesignationCategoryDef.BuildablePreceptBuilding, Designator> ___ideoBuildingDesignatorsCached)
+        {
+            foreach (var designator in designators)
+            {
+                if (designator.Label.Contains(MousekinDefOf.Mousekin_TownSquare.LabelCap))
+                { 
+                    continue;
+                }
+                yield return designator;
+            }
+        }
+    }
+}
