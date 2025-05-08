38045f54668848e9fabf53ebc0f66de2e4256e80
sumghai <sumghai@users.noreply.github.com>
2025-05-09 11:37:27 +1200

    Fix Slave Trader override patch overwriting subsequent non-slave traders

--- Source/Patches/Harmony_PawnGroupKindWorker_Trader.cs
+++ Source/Patches/Harmony_PawnGroupKindWorker_Trader.cs
@@ -1,53 +1,41 @@
 ﻿using HarmonyLib;
 using RimWorld;
+using System.Collections.Generic;
 using Verse;
 
 namespace MousekinRace
 {
-    // If a caravan trader is a slaver from the Mousekin Kingdom faction,
-    // replace the trader with a special Mousekin Slave Trader pawnkind
+    // Replace trader pawnkind in Mousekin Kingdom Slave Trader caravans with special Mousekin Slave Trader
     [HarmonyPatch(typeof(PawnGroupKindWorker_Trader), nameof(PawnGroupKindWorker_Trader.GenerateTrader))]
     public static class Harmony_PawnGroupKindWorker_Trader_GenerateTrader_OverridePawnkindForMousekinSlavers
     {
-        static void Prefix(PawnGroupMakerParms parms, ref PawnGroupMaker groupMaker, TraderKindDef traderKind)
+        // Use detour, as simply prefixing PawnGroupMaker groupMaker permanently overwrites the trader pawnkind for subsequent traders
+        static bool Prefix(PawnGroupMakerParms parms, TraderKindDef traderKind, ref Pawn __result)
         {
             if (parms.faction.ideos.PrimaryCulture.IsMousekinKingdomLike() && traderKind.category == IncidentWorker_TraderCaravanArrival.SlaverTraderKindCategory)
-            { 
-                groupMaker.traders.Clear();
-                groupMaker.traders.Add(new(){ 
-                    kind = MousekinDefOf.MousekinTraderSlaver,
-                    selectionWeight = 1,
-                });
-
+            {
+                Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(MousekinDefOf.MousekinTraderSlaver, parms.faction, PawnGenerationContext.NonPlayer, fixedIdeo: parms.ideo, tile: parms.tile, forceGenerateNewPawn: false, allowDead: false, allowDowned: false, canGeneratePawnRelations: true, mustBeCapableOfViolence: false, colonistRelationChanceFactor: 1f, forceAddFreeWarmLayerIfNeeded: false, allowGay: true, allowPregnant: false, allowFood: true, allowAddictions: true, inhabitant: parms.inhabitants));
+                pawn.mindState.wantsToTradeWithColony = true;
+                PawnComponentsUtility.AddAndRemoveDynamicComponents(pawn, actAsIfSpawned: true);
+                pawn.trader.traderKind = traderKind;
+                parms.points -= pawn.kindDef.combatPower;
+                __result = pawn;
+                return false;
             }
+            return true;
         }
     }
 
-    // If a caravan trader is a slaver from the Mousekin Kingdom faction (i.e. led by a Mousekin Slave Trader pawnkind),
-    // replace all guards with special Mousekin Slave Trader pawnkinds
+    // Curate guards for Mousekin trade caravans
+    // - Reduce number of guards
+    // - Replace all guards in Mousekin Kingdom Slave Trader caravans with special Mousekin Slave Trader pawnkinds
     [HarmonyPatch(typeof(PawnGroupKindWorker_Trader), nameof(PawnGroupKindWorker_Trader.GenerateGuards))]
     public static class Harmony_PawnGroupKindWorker_Trader_GenerateGuards_OverridePawnkindForMousekinSlavers
     {
-        static void Prefix(PawnGroupMakerParms parms, ref PawnGroupMaker groupMaker, Pawn trader)
+        // Use detour, as simply prefixing PawnGroupMaker groupMaker permanently overwrites the trader pawnkind for subsequent traders
+        static bool Prefix(PawnGroupMakerParms parms, PawnGroupMaker groupMaker, Pawn trader, ref List<Pawn> outPawns)
         {
-            if (parms.faction.ideos.PrimaryCulture.IsMousekinKingdomLike() && trader.kindDef == MousekinDefOf.MousekinTraderSlaver)
-            {
-                groupMaker.guards.Clear();
-                groupMaker.guards.Add(new()
-                {
-                    kind = MousekinDefOf.MousekinTraderSlaver,
-                    selectionWeight = 1,
-                });
-            }
-        }
-    }
-
-    // Reduce number of guards for Mousekin caravan traders
-    [HarmonyPatch(typeof(PawnGroupKindWorker_Trader), nameof(PawnGroupKindWorker_Trader.GenerateGuards))]
-    public static class Harmony_PawnGroupKindWorker_Trader_GenerateGuards_ReduceNumberForMousekinTraders
-    {
-        static void Prefix(ref PawnGroupMakerParms parms, Pawn trader)
-        {
+            // Reduce number of guards (always run)
             if (parms.faction.ideos.PrimaryCulture.IsMousekin())
             {
                 parms.points = Rand.Range(200, 250);
@@ -56,6 +44,17 @@ namespace MousekinRace
                     parms.points = Rand.Range(80, 120);
                 }
             }
+
+            if (parms.faction.ideos.PrimaryCulture.IsMousekinKingdomLike() && trader.kindDef == MousekinDefOf.MousekinTraderSlaver)
+            {
+                foreach (PawnGenOptionWithXenotype item2 in PawnGroupMakerUtility.ChoosePawnGenOptionsByPoints(parms.points, groupMaker.guards, parms))
+                {
+                    Pawn pawn = PawnGenerator.GeneratePawn(new PawnGenerationRequest(MousekinDefOf.MousekinTraderSlaver, parms.faction, PawnGenerationContext.NonPlayer, fixedIdeo: parms.ideo, tile: parms.tile, forceGenerateNewPawn: false, allowDead: false, allowDowned: false, canGeneratePawnRelations: true, mustBeCapableOfViolence: false, colonistRelationChanceFactor: 1f, forceAddFreeWarmLayerIfNeeded: false, allowGay: true, allowPregnant: false, allowFood: true, allowAddictions: true, inhabitant: parms.inhabitants));
+                    outPawns.Add(pawn);
+                }
+                return false;
+            }
+            return true;
         }
     }
 }
