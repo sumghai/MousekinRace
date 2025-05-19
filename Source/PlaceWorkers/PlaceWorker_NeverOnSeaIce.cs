9cea502aa2c560ac7891e113ac4fc493d9be07cb
sumghai <sumghai@users.noreply.github.com>
2025-05-19 19:24:54 +1200

    Prevent Mine Entrances being built on sea ice terrain

---
+++ Source/PlaceWorkers/PlaceWorker_NeverOnSeaIce.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MousekinRace
+{
+    public class PlaceWorker_NeverOnSeaIce : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            if (loc.GetTerrain(map) == TerrainDefOf.Ice)
+            { 
+                return "MousekinRace_PlaceWorker_NeverOnSeaIce".Translate(checkingDef.LabelCap);
+            }
+            return true;
+        }
+    }
+}
