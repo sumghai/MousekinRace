2fdb8b54f6e3f51c2f404c457dc808f6ccc9fc50
sumghai <sumghai@users.noreply.github.com>
2025-10-08 21:18:54 +1300

    (WIP) Add Mousekin tales (novels) and textbooks

---
+++ Source/DefModExtensions/BookNameDescByGenreExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Verse;
+
+namespace MousekinRace
+{
+    public class BookNameDescByGenreExtension : DefModExtension
+    {
+        public List<BookNameDescPair> nameDescPairs;
+    }
+
+    public class BookNameDescPair
+    {
+        public RulePackDef nameMaker;
+
+        public RulePackDef descriptionMaker;
+
+        public float weight = 1f;
+    }
+}
---
+++ Source/Patches/Harmony_Book.cs
@@ -0,0 +1,74 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Verse;
+
+namespace MousekinRace
+{
+    // If a book def contains a special mod extension that supports multiple Name and Description Makers by genre,
+    // pick a random Name/Description pair to generate the book's contents with
+    [HarmonyPatch(typeof(Book), nameof(Book.GenerateBook))]
+    public static class Harmony_Book_GenerateBook_ChooseFromMultipleNameDescPairs
+    {
+        static RulePackDef storedRandDescMaker;
+        
+        [HarmonyTranspiler]
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codeMatcher = new CodeMatcher(instructions);
+
+            CodeMatch[] toMatch =
+            [
+                new CodeMatch(OpCodes.Call),
+                new CodeMatch(OpCodes.Ldarg_0),
+                new CodeMatch(OpCodes.Call),
+                new CodeMatch(OpCodes.Callvirt),
+                new CodeMatch(OpCodes.Ldfld)
+            ];
+
+            CodeInstruction[] toInsert_condReplaceNameMaker =
+            [
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_Book_GenerateBook_ChooseFromMultipleNameDescPairs), nameof(ConditionallyReplaceNameMaker)))
+            ];
+
+            CodeInstruction[] toInsert_condReplaceDescMaker =
+            [
+                new CodeInstruction(OpCodes.Ldarg_0),
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(Harmony_Book_GenerateBook_ChooseFromMultipleNameDescPairs), nameof(ConditionallyReplaceDescMaker)))
+            ];
+
+            // Conditionally replace name maker
+            codeMatcher.MatchEndForward(toMatch);
+            codeMatcher.InsertAfter(toInsert_condReplaceNameMaker);
+
+            // Conditionally replace description maker
+            codeMatcher.MatchEndForward(toMatch);
+            codeMatcher.InsertAfter(toInsert_condReplaceDescMaker);
+
+            return codeMatcher.InstructionEnumeration();
+        }
+
+        private static RulePackDef ConditionallyReplaceNameMaker(RulePackDef originalNameMaker, Book book)
+        {
+            if (book.def.HasModExtension<BookNameDescByGenreExtension>() && !book.def.GetModExtension<BookNameDescByGenreExtension>().nameDescPairs.NullOrEmpty())
+            {
+                BookNameDescPair randBookNameDescPair = book.def.GetModExtension<BookNameDescByGenreExtension>().nameDescPairs.RandomElementByWeight(x => x.weight);
+                storedRandDescMaker = randBookNameDescPair.descriptionMaker;
+                return randBookNameDescPair.nameMaker;
+            }
+
+            return originalNameMaker;
+        }
+
+        private static RulePackDef ConditionallyReplaceDescMaker(RulePackDef originalDescMaker, Book book)
+        {
+            if (book.def.HasModExtension<BookNameDescByGenreExtension>() && !book.def.GetModExtension<BookNameDescByGenreExtension>().nameDescPairs.NullOrEmpty() && storedRandDescMaker != null)
+            {
+                return storedRandDescMaker;
+            }
+
+            return originalDescMaker;
+        }
+    }
+}
