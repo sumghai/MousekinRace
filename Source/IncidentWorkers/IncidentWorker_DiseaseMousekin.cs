d52df4968b1005a95ed5b9e59a9134ed4c683566
sumghai <sumghai@users.noreply.github.com>
2025-11-01 22:17:18 +1300

    Add ear rot disease and incident

---
+++ Source/IncidentWorkers/IncidentWorker_DiseaseMousekin.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace MousekinRace
+{
+    public class IncidentWorker_DiseaseMousekin : IncidentWorker_DiseaseHuman
+    {
+        public override IEnumerable<Pawn> PotentialVictimCandidates(IIncidentTarget target)
+        {
+            if (target is Map map)
+            {
+                return map.mapPawns.FreeColonistsAndPrisoners.Where((Pawn x) => (x.ParentHolder == null || x.ParentHolder is not CompBiosculpterPod) && x.IsMousekin());
+            }
+            return ((Caravan)target).PawnsListForReading.Where((Pawn x) => (x.IsFreeColonist || x.IsPrisonerOfColony) && x.IsMousekin());
+        }
+    }
+}
---
+++ Source/IngestionOutcomeDoers/IngestionOutcomeDoer_RemoveHediff.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using Verse;
+
+namespace MousekinRace
+{
+    public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+    {
+        public HediffDef hediffDef;
+
+        public override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            if (hediffDef != null) 
+            {
+                pawn.health.hediffSet.hediffs.RemoveAll(x => x.def == hediffDef);
+            }
+        }
+    }
+}
