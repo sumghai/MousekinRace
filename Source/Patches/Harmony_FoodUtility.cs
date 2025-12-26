574f1ca8384178ae98846a4a0c000ab177a6966b
sumghai <sumghai@users.noreply.github.com>
2025-12-26 20:32:11 +1300

    Fix root cellar animal feeding patch breaking taming

--- Source/Patches/Harmony_FoodUtility.cs
+++ Source/Patches/Harmony_FoodUtility.cs
@@ -8,11 +8,11 @@ namespace MousekinRace
     [HarmonyPatch(typeof(FoodUtility), nameof(FoodUtility.WillEat), [typeof(Pawn), typeof(Thing), typeof(Pawn), typeof(bool), typeof(bool)])]
     public static class Harmony_FoodUtility_WillEat_AnimalsSkipFoodInCellars
     {
-        static void Postfix(ref bool __result, Pawn p, Thing food)
+        static void Postfix(ref bool __result, Pawn p, Thing food, Pawn getter)
         {
             // Make sure to only prohibit food stored in the cellar's storage cell
             // (food on any other cell occupied by the cellar is considered "outside" the cellar, and thus valid)
-            if ((p.IsAnimal || p.IsWildMan()) && food.Position.GetFirstBuilding(food.Map) is Building_CellarOutdoor cellar && food.Position == cellar.StorageCellPos)
+            if (((getter?.IsAnimal ?? false) || (getter?.IsWildMan() ?? false)) && food.IsInAnyStorage() && food.PositionHeld.GetFirstBuilding(food.Map) is Building_CellarOutdoor cellar && food.PositionHeld == cellar.StorageCellPos)
             {
                 __result = false;
             }
